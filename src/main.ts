9d62d55741dc4a001d2cf03c2775b1669373612b
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-12-31 02:18:25 +0300

    fix(common): entrypoint

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ const CANVAS_ID = "channels-canvas";
 const IMAGE_SRC = "assets/flowers.jpg";
 
 const bootstrap = () => {
-  const composition = new Composition(CANVAS_ID, IMAGE_SRC);
+  const composition = new Composition({
+    canvasId: CANVAS_ID,
+    imageSrc: IMAGE_SRC,
+  });
   composition.init();
 };
 
