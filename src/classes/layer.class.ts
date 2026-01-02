e1fe8b34e38db07f2ef1bd5151898da8d36c992f
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-01-02 11:25:53 +0300

    feat(layer): set blend mode method

--- src/classes/layer.class.ts
+++ src/classes/layer.class.ts
@@ -1,10 +1,17 @@
+import { BlendMod } from "./composition.interfaces";
 import type { LayerOptions } from "./layer.interfaces";
+
 export class Layer {
-  options?: LayerOptions;
+  options?: LayerOptions = {};
   data: Uint8ClampedArray;
 
-  constructor(data?: Uint8ClampedArray, options?: LayerOptions) {
-    this.data = data || new Uint8ClampedArray();
+  constructor(data: Uint8ClampedArray, options?: LayerOptions) {
+    this.data = data;
     this.options = options;
+  }
+
+  setBlendMode(blendMod: BlendMod) {
+    if (!this.options) this.options = {};
+    this.options.blendMod = blendMod;
   }
 }
