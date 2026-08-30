66779b44f2d58aa59f213b942a3b92c2facc8a35
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-08-30 14:55:34 +0300

    feat(toasts): toast 1 process

--- packages/toasts/src/toasts/toast-1_degas/index.ts
+++ packages/toasts/src/toasts/toast-1_degas/index.ts
@@ -28,11 +28,25 @@ console.log("[toast-1/degas] input:", inputPath);
 const { data, width, height } = await imageFileToRawData(inputPath);
 
 const composition = new Composition(width, height);
-const whiteLayer = composition.createBlankLayer();
-const whiteColor = Color.fromHex("#FFFFFF");
-whiteLayer.fill(whiteColor);
-const layer = composition.createLayerFromPixelData(data);
-layer.mask({ name: "hue", value: 240, tolerance: 0.1 }); // red hue, 0°
+
+const darkGrayGackgroundLayer = composition.createBlankLayer();
+const darkGrayBackgroudColor = Color.fromHex("#a4a4a4");
+darkGrayGackgroundLayer.fill(darkGrayBackgroudColor);
+
+const bluredLayer = composition.createLayerFromPixelData(data);
+bluredLayer.applyEffect({ name: "blur", options: { radius: 2 } });
+bluredLayer.setOpacity(0.6);
+
+const lightGrayBackgroundLayer = composition.createBlankLayer();
+const lightGrayBackgroundColor = Color.fromHex("#ebebeb");
+lightGrayBackgroundLayer.fill(lightGrayBackgroundColor);
+lightGrayBackgroundLayer.setOpacity(0.8);
+
+const blueBackgroundLayer = composition.createBlankLayer();
+const blueBackgroundColor = Color.fromHex("#00ffdd");
+blueBackgroundLayer.fill(blueBackgroundColor);
+blueBackgroundLayer.setOpacity(0.8);
+blueBackgroundLayer.setBlendMode("lch-hue");
 
 const result = composition.render();
 await rawDataToImageFile(result, { width, height }, outputPath);
