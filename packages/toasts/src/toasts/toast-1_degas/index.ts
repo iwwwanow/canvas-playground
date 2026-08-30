a42b63cdff4bd78d85abae6486ebcfa31782ab8b
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-08-30 17:39:11 +0300

    feat(toasts): toast 1 static done

--- packages/toasts/src/toasts/toast-1_degas/index.ts
+++ packages/toasts/src/toasts/toast-1_degas/index.ts
@@ -39,6 +39,17 @@ const getTransformCorners = (gapModifier: number = 0.1) => {
   return corners;
 };
 
+const getBlurRadius = (blurModifier: number): number => {
+  return Math.round((width * blurModifier) / 100); // 1% от ширины
+};
+
+const getTransformParams = ({ tx, ty }) => {
+  return {
+    tx: Math.round(tx * width * 0.01),
+    ty: Math.round(ty * height * 0.01),
+  };
+};
+
 const { data, width, height } = await imageFileToRawData(inputPath);
 
 const composition = new Composition(width, height);
@@ -46,15 +57,18 @@ const composition = new Composition(width, height);
 const darkGrayGackgroundLayer = composition.createBlankLayer();
 const darkGrayBackgroudColor = Color.fromHex("#a4a4a4");
 darkGrayGackgroundLayer.fill(darkGrayBackgroudColor);
-//
+
 const bluredLayer = composition.createLayerFromPixelData(data);
-bluredLayer.applyEffect({ name: "blur", options: { radius: 2 } });
-bluredLayer.setOpacity(0.6);
+bluredLayer.applyEffect({
+  name: "blur",
+  options: { radius: getBlurRadius(0.8) },
+});
+bluredLayer.setOpacity(0.8);
 
 const lightGrayBackgroundLayer = composition.createBlankLayer();
 const lightGrayBackgroundColor = Color.fromHex("#ebebeb");
 lightGrayBackgroundLayer.fill(lightGrayBackgroundColor);
-lightGrayBackgroundLayer.setOpacity(0.8);
+lightGrayBackgroundLayer.setOpacity(0.6);
 
 const blueBackgroundLayer = composition.createBlankLayer();
 const blueBackgroundColor = Color.fromHex("#00ffdd");
@@ -64,34 +78,51 @@ blueBackgroundLayer.setBlendMode("lch-hue");
 
 const purpleLayer = composition.createLayerFromPixelData(data);
 const purpleColor = Color.fromHex("#FF00FF");
-purpleLayer.mask({ name: "value", value: 10, tolerance: 0.6 });
+purpleLayer.mask({ name: "value", value: 16, tolerance: 0.32 });
 purpleLayer.tint(purpleColor);
-purpleLayer.applyEffect({ name: "blur", options: { radius: 0.4 } });
-purpleLayer.setOpacity(0.32);
+purpleLayer.applyEffect({
+  name: "blur",
+  options: { radius: getBlurRadius(0.24) },
+});
+purpleLayer.setOpacity(0.6);
 purpleLayer.setTransform({
   name: "perspective",
   params: { corners: getTransformCorners() },
 });
+purpleLayer.setTransform({
+  name: "translate",
+  params: getTransformParams({ tx: 1, ty: -1 }),
+});
 
 const redLayer = composition.createLayerFromPixelData(data);
 const redColor = Color.fromHex("#FF0000");
-redLayer.mask({ name: "value", value: 10, tolerance: 0.3 });
+redLayer.mask({ name: "value", value: 12, tolerance: 0.24 });
 redLayer.tint(redColor);
-redLayer.applyEffect({ name: "blur", options: { radius: 0.2 } });
-redLayer.setOpacity(0.6);
+redLayer.applyEffect({
+  name: "blur",
+  options: { radius: getBlurRadius(0.1) },
+});
+redLayer.setOpacity(0.8);
 redLayer.setTransform({
   name: "perspective",
   params: { corners: getTransformCorners(0.2) },
 });
+redLayer.setTransform({
+  name: "translate",
+  params: getTransformParams({ tx: 2, ty: -2 }),
+});
 
-const orrangeLayer = composition.createLayerFromPixelData(data);
-const orrangeColor = Color.fromHex("#ff8000");
-orrangeLayer.mask({ name: "value", value: 8, tolerance: 0.24 });
-orrangeLayer.tint(orrangeColor);
-orrangeLayer.setOpacity(0.8);
-orrangeLayer.setTransform({
+const whiteLayer = composition.createLayerFromPixelData(data);
+const whiteColor = Color.fromHex("#FFFFFF");
+whiteLayer.mask({ name: "value", value: 92, tolerance: 0.16 });
+whiteLayer.tint(whiteColor);
+whiteLayer.setTransform({
   name: "perspective",
-  params: { corners: getTransformCorners(0.4) },
+  params: { corners: getTransformCorners(0.1) },
+});
+whiteLayer.setTransform({
+  name: "translate",
+  params: getTransformParams({ tx: 1, ty: -1 }),
 });
 
 const result = composition.render();
